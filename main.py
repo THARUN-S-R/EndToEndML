a92c4ffdb4ef920552eb1828d4990d6ad3bf2a48
Tharun <tharun.s.r.599172@gmail.com>
2025-04-07 00:41:56 +0100

    Data Validation Changes added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from EndToEndProject import logger
 from EndToEndProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from EndToEndProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -8,6 +9,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     Data_Ingestion  = DataIngestionTrainingPipeline()
     Data_Ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e                                     
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    Data_Validation  = DataValidationTrainingPipeline()
+    Data_Validation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
---
+++ src/EndToEndProject/components/data_validation.py
@@ -0,0 +1,41 @@
+import os
+from EndToEndProject import logger
+import pandas as pd
+from EndToEndProject.entity.config_entity import DataValidationConfig
+
+
+class DataValidation:
+    def __init__(self, config: DataValidationConfig):
+        self.config = config
+
+
+    def validate_all_columns(self)-> bool:
+        try:
+            validation_status = None
+
+            data = pd.read_csv(self.config.unzip_data_dir)
+            all_cols = list(data.columns)
+            types = data.dtypes
+
+            all_schema = self.config.all_schema.keys()
+
+            
+            for col in all_cols:
+                if col not in all_schema:
+                    validation_status = False
+                    with open(self.config.STATUS_FILE, 'w') as f:
+                        f.write(f"Validation status: {validation_status}")
+                elif types[col] != self.config.all_schema[col]:
+                    validation_status = False
+                    with open(self.config.STATUS_FILE, 'w') as f:
+                        f.write(f"Validation status: {validation_status}")
+                else:
+                    validation_status = True
+                    with open(self.config.STATUS_FILE, 'w') as f:
+                        f.write(f"Validation status: {validation_status}")
+
+            return validation_status
+        
+        except Exception as e:
+            raise e
+
